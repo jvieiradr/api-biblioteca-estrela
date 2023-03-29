18d37d79fda90e92ec8d8c9f7bb237c242a44e53
José Vieira Costa Júnior <jvieiradr1008@gmail.com>
2023-03-29 00:13:02 -0300

    terceiro

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 import mysql from 'mysql';
 
 const banco = mysql.createConnection({
+    /*
     host: 'aws-sa-east-1.connect.psdb.cloud',
     user: 's3gyfl4noz6uuoqs45gl',
     password: 'pscale_pw_MR7jSjsydalorLcg12VVxwPCmG2UTpNyJBH73ovKDN8',
     database: 'biblioteca',
     ssl : {
         rejectUnauthorized: false
-    }
-    /*
+    }*/
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'biblioteca'*/
+    database: 'biblioteca'
 });
 
 export default banco;
